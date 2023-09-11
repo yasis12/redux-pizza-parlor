7792cf3b6e08daf42f91e6e5d94e91bccccd3111
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-11 18:15:57 -0500

    Set up redux store in application

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+//Redux imports
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import logger from 'redux-logger';
+
+// Reducer pizza selector
+const pizzaSelector = (state = [], action) => {
+    if(action.type === 'SET_PIZZA') {
+
+      return action.payload
+    }
+    return state;
+  }
+// Reducer Check out
+
+
+
+// Reducer Customer info
+
+
+
+
+// Redux Store
+const reduxStore = createStore(
+    combineReducers({
+        pizzaSelector
+    }),
+    applyMiddleware(logger)
+  );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={reduxStore}>
+          <App />
+        </Provider>
     </React.StrictMode>
 );
