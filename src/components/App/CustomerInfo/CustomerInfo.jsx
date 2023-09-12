acde866348df1c3a2b3709910e914c71e633742d
Jeff Walter <Jeff.Walter@me.com>
2023-09-11 19:21:44 -0500

    created addCustomer function

--- src/components/App/CustomerInfo/CustomerInfo.jsx
+++ src/components/App/CustomerInfo/CustomerInfo.jsx
@@ -4,6 +4,49 @@ import axios from 'axios';
 
 function CustomerInfo(){
 
+let [customerToAdd, setCustomerToAdd] = useState({ name: '' , streetAddress: '', city: '', zip: '' });
+
+const handleNameChange = (event) => {
+  setCustomerToAdd({
+    ...customerToAdd,
+    name: event.target.value,
+  });
+}
+
+const handleAddressChange = (event) => {
+  setCustomerToAdd({
+    ...customerToAdd,
+    streetAddress: event.target.value,
+  });
+}
+
+const handleCityChange = (event) => {
+  setCustomerToAdd({
+    ...customerToAdd,
+    city: event.target.value,
+  });
+}
+
+const handleZipChange = (event) => {
+  setCustomerToAdd({
+    ...customerToAdd,
+    zip: event.target.value,
+  });
+}
+
+const addCustomer = (event) => {
+  event.preventDefault();
+  
+  axios.post('/CustomerInfo', customerToAdd)
+  .then((response) => {
+    setCustomerToAdd({ name: '', streetAddress: '', city: '', zip: '' })
+  })
+  .catch((error) => {
+    console.log(error);
+    alert('customerInfo post error');
+  });
+};
+
 
 
 }
