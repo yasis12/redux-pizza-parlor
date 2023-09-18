d59e6a7ef31f3ec8d9372aca149c2bb02cf7cf84
Jeff Walter <Jeff.Walter@me.com>
2023-09-18 18:54:03 -0500

    added css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import './App.css';
-import CustomerInfo from './CustomerInfo/CustomerInfo';
-import SelectPizza from './SelectPizza/SelectPizza';
+import CustomerInfo from '../CustomerInfo/CustomerInfo';
+import SelectPizza from '../SelectPizza/SelectPizza';
+import Checkout from '../Checkout/Checkout';
 
 // React Imports
 import React from 'react';
@@ -28,6 +29,15 @@ function App() {
 
           <Route path="/SelectPizza" >
               <SelectPizza />
+          </Route>
+
+        {/*temproary link to get to checkout faster- will get removed.  */}
+          <p>
+              <Link to="/Checkout"> Checkout</Link>
+          </p>
+
+          <Route path="/Checkout" >
+              <Checkout  />
           </Route>
       </Router>
 
